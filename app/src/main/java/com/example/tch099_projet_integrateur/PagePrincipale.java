32a3d2b3590bc9565ee684a80b9686ea8c8ba2b2
Felix Caron <felix.caron.2@ens.etsmtl.ca>
2024-03-01 17:03:38 -0500

    Ajout du logo et titre sur le header

--- app/src/main/java/com/example/tch099_projet_integrateur/PagePrincipale.java
+++ app/src/main/java/com/example/tch099_projet_integrateur/PagePrincipale.java
@@ -7,12 +7,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.View;
 
-public class DemanderSupport extends AppCompatActivity {
+public class PagePrincipale extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demander_support);
+        setContentView(R.layout.activity_page_principale);
 
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
 
