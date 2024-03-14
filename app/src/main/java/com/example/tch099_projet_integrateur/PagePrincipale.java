26771e5a3934851f5dce862049c2203a6a2b7134
Felix Caron <felix.caron.2@ens.etsmtl.ca>
2024-03-14 15:12:53 -0400

    Debut des intentions

--- app/src/main/java/com/example/tch099_projet_integrateur/PagePrincipale.java
+++ app/src/main/java/com/example/tch099_projet_integrateur/PagePrincipale.java
@@ -1,20 +1,35 @@
 package com.example.tch099_projet_integrateur;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.TaskStackBuilder;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import java.util.Objects;
+
 public class PagePrincipale extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_principale);
 
+
+        //Active la barre de navigation
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
 
         findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +37,40 @@ public class PagePrincipale extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+
+        MenuItem support = menu.findItem(R.id.menuSupport);
+
+        MenuItem virementCompte = findViewById(R.id.compte);
+        MenuItem depot = findViewById(R.id.menuDepot);
+        MenuItem facture = findViewById(R.id.menuFacture);
+        MenuItem notification = findViewById(R.id.menuNotification);
+
+
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Log.i("MesMessages", "YUP");
+
+        if (item.getItemId() == R.id.menuSupport) {
+            Intent s = new Intent(this, SupportNautico.class);
+            startActivity(s);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
 }
