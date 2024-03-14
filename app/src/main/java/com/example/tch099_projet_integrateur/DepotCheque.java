6ef6c5d2064ff23a1a7d8c01c5ac9dcdbdc76490
Felix Caron <felix.caron.2@ens.etsmtl.ca>
2024-03-14 12:28:49 -0400

    Ajout activité DepotCheque

---
+++ app/src/main/java/com/example/tch099_projet_integrateur/DepotCheque.java
@@ -0,0 +1,14 @@
+package com.example.tch099_projet_integrateur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepotCheque extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_depot_cheque);
+    }
+}
