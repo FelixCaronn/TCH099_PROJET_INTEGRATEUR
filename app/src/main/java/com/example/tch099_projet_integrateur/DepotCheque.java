3825878ddac1d1521dbfe2644d1ce0ab71a77cb2
JonathanEts3000 <jonathan.mongrain.1@ens.etsmtl.ca>
2024-03-15 15:44:12 -0400

    implementation ocr

--- app/src/main/java/com/example/tch099_projet_integrateur/DepotCheque.java
+++ app/src/main/java/com/example/tch099_projet_integrateur/DepotCheque.java
@@ -1,22 +1,19 @@
 package com.example.tch099_projet_integrateur;
 
 import static com.example.tch099_projet_integrateur.PagePrincipale.openDrawer;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.app.Activity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -26,7 +23,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -35,26 +31,31 @@ import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
-
 import java.io.FileNotFoundException;
 
-public class DepotCheque extends AppCompatActivity {
+public class DepotCheque extends AppCompatActivity implements View.OnClickListener {
     DrawerLayout drawerLayout;
     ImageView menu;
     LinearLayout home, depot, facture, notification, support, transfertClient, transfertCompte;
-
-    Uri textTargetUri;
-    ImageView targetImage;
-    ImageView verso;
+    ImageView recto, verso;
+    EditText montantDepot;
+    Button deposer;
+    Uri uriRecto = null;
+    Uri uriVerso = null;
+    @Override
+    public void onClick(View v){
+        if (uriRecto!=null && uriVerso!=null && montantDepot.getText().toString()!=""){
+            deposer.setEnabled(true);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_depot_cheque);
 
+        montantDepot = findViewById(R.id.editTextMontantDepot);
         drawerLayout = findViewById(R.id.drawerLayout);
-
         menu = findViewById(R.id.menu);
-
         home = findViewById(R.id.home);
         depot = findViewById(R.id.depot);
         facture = findViewById(R.id.facture);
@@ -62,8 +63,26 @@ public class DepotCheque extends AppCompatActivity {
         support = findViewById(R.id.support);
         transfertClient = findViewById(R.id.transfertClient);
         transfertCompte = findViewById(R.id.transfertCompte);
-
-
+        recto = findViewById(R.id.imgRecto);
+        verso = findViewById(R.id.imgVerso);
+        deposer = findViewById(R.id.buttonDepot);
+        deposer.setEnabled(false);
+        recto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 69);
+            }
+        });
+        verso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 70);
+            }
+        });
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,9 +103,47 @@ public class DepotCheque extends AppCompatActivity {
                 redirectActivity(DepotCheque.this, PagePrincipale.class);
             }
         });
-
+        deposer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uriRecto));
+                    Bitmap resizedBmp = Bitmap.createBitmap(bitmap, 1780, 350, 230, 70);
+                    InputImage image = InputImage.fromBitmap(resizedBmp, 0);
+                    TextRecognizer recognizer;
+                    recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+                    //Bitmap resizedBmp = Bitmap.createBitmap(bitmap,(int)(0.81315*bitmap.getWidth()),(int)(0.35389*bitmap.getHeight()),(int)(0.1050708*bitmap.getWidth()),(int)(0.07077*bitmap.getHeight()));
+                    //dimension image = 2189x989
+                    Task<Text> result =
+                            recognizer.process(image)
+                                    .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                        @Override
+                                        public void onSuccess(Text visionText) {
+                                            // Task completed successfully
+                                            // ...
+                                            String resultText = visionText.getText();
+                                            if (Double.parseDouble(resultText) == Double.parseDouble(montantDepot.getText().toString())){
+                                                Toast.makeText(DepotCheque.this, "Dépot approuvé!", Toast.LENGTH_LONG).show();
+                                            }
+                                            else{
+                                                Toast.makeText(DepotCheque.this, "Le montant inscris ne coïncide pas avec celui sur le chèque!", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    })
+                                    .addOnFailureListener(
+                                            new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(DepotCheque.this, "Photo invalide", Toast.LENGTH_LONG).show();
+                                                }
+                                            });
+                }catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-
     public static void openDrawer(DrawerLayout drawerLayout){
         drawerLayout.openDrawer(GravityCompat.START);
     }
@@ -101,13 +158,41 @@ public class DepotCheque extends AppCompatActivity {
         activity.startActivity(intent);
         activity.finish();
     }
-
     @Override
     protected void onPause() {
         super.onPause();
         closeDrawer(drawerLayout);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == 69){
+            if (resultCode == RESULT_OK){
+                uriRecto = data.getData();
+                Bitmap bitmap;
+                try {
+                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uriRecto));
+                    recto.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        else if (requestCode == 70){
+            if (resultCode == RESULT_OK){
+                uriVerso = data.getData();
+                Bitmap bitmap;
+                try {
+                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uriVerso));
+                    verso.setImageBitmap(bitmap);
+                }
+                catch (FileNotFoundException e){}
+            }
+        }
+    }
 
 
 
